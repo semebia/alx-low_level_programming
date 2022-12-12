4faabfd1b57caf1989f0ada5180753f703df0732
semebia <semebiawurah56@gmail.com>
2022-12-12 02:04:00 +0000

    task 2-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main- Print base 10 numbers
+ * void: empty parameter list for main
+ * description: prints numbers 1-9
+ * return: 0 for success
+ */
+
+
+int main(void)
+{
+	char num = '0';
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
