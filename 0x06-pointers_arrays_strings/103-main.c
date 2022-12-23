1a64f15b4133e7ca18f3f83c2a2847848be80b76
semebia <semebiawurah56@gmail.com>
2022-12-23 00:24:12 +0000

    task 0-task 8, unsure of task 5; may do it again

---
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *n = "123456789243457436782357457567847768578564568587687677458673";
+	char *m = "903479066347069723468291456934625963495869324659732465976234";
+	char r[100];
+	char r2[10];
+	char r3[11];
+	char *res;
+
+	res = infinite_add(n, m, r, 100);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	n = "1234567890";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	n = "999999999";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	res = infinite_add(n, m, r3, 11);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+return (0);
+}
