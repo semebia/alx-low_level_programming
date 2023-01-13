689c95e6b6b31b2ecbe2f9fdbf332da0c61a032a
semebia <semebiawurah56@gmail.com>
2023-01-13 09:02:21 +0000

    task 1 update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,54 +1,40 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-/**
- * _strlen - returns the length of the string
- * @s: input string to count
- * Description: returns the length of a given string
- * Return: length of string as int
- **/
-int _strlen(char *s)
-{
-	unsigned int i;
+#include <main.h>
 
-	if (s == NULL)
-		return (1);
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i + 1);
-}
 /**
- * string_nconcat - concatenate n chars of second string to first
- * @s1: first string
- * @s2: second string
- * @n: number of chars to concatenate
- * Description: concatenate two strings, return NULL if fails
- * Return: pointer to string, NULL if fails
- **/
+ *string_nconcat - concatenates n bytes of a string to another string
+ *@s1: string to append to
+ *@s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ * *Return: pointer to the resulting string
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *dest;
-	unsigned int size1, size2, i, j;
-
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	if (n > size2 - 1)
-		n = size2 - 1;
-
-	dest = (char *)malloc((size1 + n) * sizeof(char));
-	if (dest == NULL)
+	char *s;
+	
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 		return (NULL);
-	if (size1 == 1 && size2 == 1)
-		return (NULL);
-
-	for (i = 0; i < size1 - 1; i++)
-		dest[i] = s1[i];
-
-	for (j = 0; j <= n; j++)
+	while (i < len1)
 	{
-		dest[i + j] = s2[j];
+		s[i] = s1[i];
+		i++;
 	}
-	dest[i + j] = '\0';
-
-	return (dest);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
+
